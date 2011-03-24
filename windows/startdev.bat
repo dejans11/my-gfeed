4e6de744cda492b9d88739cf7b476ce5ab87fefd
Dejan <dejans11@gmail.com>
2011-03-24 19:46:27 +0200

    Added windows batch files for uploading app and starting development server.

---
+++ windows/startdev.bat
@@ -0,0 +1,1 @@
+python "C:/Program Files/Google/google_appengine/dev_appserver.py" --port=1111 "../src"
---
+++ windows/upload_app.bat
@@ -0,0 +1,4 @@
+set HTTP_PROXY=http://squid.eurodyn.com:8080
+set HTTPS_PROXY=http://squid.eurodyn.com:8080
+
+python "C:/Program Files/Google/google_appengine/appcfg.py" update "../src"
